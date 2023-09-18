eaad8c2ec00cba5e337d433d3f710b0100a68ce2
tanvir/156 <rana525203@gmail.com>
2023-09-19 00:29:54 +0600

    server updated

--- backend/server.js
+++ backend/server.js
@@ -27,8 +27,8 @@ app.get("/", (req, res) => {
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "http://localhost:3000"
-    // "https://bid-website-frontend-l3on-irxdefx17-tanvir156.vercel.app"
+    // "http://localhost:3000"
+    "https://bid-website-frontend-l3on-irxdefx17-tanvir156.vercel.app"
   ); // Allow requests from this origin
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Allow specific HTTP methods
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow specific headers
