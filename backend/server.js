7f85377a6f21a0cc386c3fddf335374ce3f4bd40
Tanvir156 <72537009+Tanvir156@users.noreply.github.com>
2023-09-21 01:58:31 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
     // "http://localhost:3000"
-    "https://bid-website-frontend-l3on.vercel.app"
+    "https://bid-website-frontend.vercel.app"
   ); // Allow requests from this origin
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Allow specific HTTP methods
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow specific headers
